0f4810cf42c8cb7888a474731d6bf7f0edacfba9
Ju Ann <juanneriksen@gmail.com>
2025-04-27 22:27:39 +0200

    animated items

--- script/script.js
+++ script/script.js
@@ -1,4 +1,29 @@
-gsap.registerPlugin( ScrollTrigger );
+// make the text boxes appear
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate');
+    } else {
+      entry.target.classList.remove('animate');
+    }
+  });
+}, { threshold: 0.5 });
+
+const texts = document.querySelectorAll('.scene__text');
+
+texts.forEach(text => {
+  observer.observe(text);
+});
+
+
+
+// idk if working
+const images = document.querySelectorAll('img');
+images.forEach(image => {
+    observer.observe(image);
+});
+
+gsap.registerPlugin(ScrollTrigger);
 
 gsap.to("#firework1", {
     scrollTrigger: {
@@ -6,23 +31,88 @@ gsap.to("#firework1", {
         toggleActions: "restart pause reverse pause"
     },
     y: -100,
+    opacity: 1,
+    scale: 2,
     rotation: 360,
     duration: 3
 });
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('animate');
-      } else {
-        entry.target.classList.remove('animate');
-      }
-    });
-  }, { threshold: 0.5 });
-  
-  const texts = document.querySelectorAll('.scene__text');
-  
-  texts.forEach(text => {
-    observer.observe(text);
-  });
-  
+
+gsap.to("#firework2", {
+  scrollTrigger: {
+      trigger: "#firework2",
+      toggleActions: "restart pause reverse pause"
+  },
+  y: -150,
+  opacity: 1,
+  scale:2,
+  rotation: 360,
+  duration: 5
+});
+
+gsap.to("#gamecube", {
+  scrollTrigger: {
+      trigger: "#gamecube",
+      toggleActions: "restart pause reverse pause"
+  },
+  x: -350,
+  duration: 3
+});
+
+gsap.to("#car", {
+  scrollTrigger: {
+      trigger: "#car",
+      toggleActions: "restart pause reverse pause"
+  },
+  x: -390,
+  duration: 3
+});
+
+gsap.to("#person", {
+  scrollTrigger: {
+      trigger: "#person",
+      toggleActions: "restart pause reverse pause"
+  },
+  opacity: 1,
+  duration: 5
+});
+
+gsap.to("#crown", {
+  scrollTrigger: {
+      trigger: "#crown",
+      toggleActions: "restart pause reverse pause"
+  },
+  opacity: 1,
+  duration: 5,
+  delay: 1
+});
+
+gsap.to("#castle", {
+  scrollTrigger: {
+      trigger: "#castle",
+      toggleActions: "restart pause reverse pause"
+  },
+  opacity: 1,
+  duration: 5,
+  delay:1.5
+});
+
+gsap.to("#viking-ship", {
+  scrollTrigger: {
+      trigger: "#viking-ship",
+      toggleActions: "restart pause reverse pause"
+  },
+  x: -390,
+  duration: 3
+});
+
+gsap.to("#bottle", {
+  scrollTrigger: {
+      trigger: "#bottle-effect",
+      end: "80% 92%",
+      markers: true,
+      toggleActions: "restart resart pause reset"
+  },
+  opacity: 0,
+  duration: 3
+});
